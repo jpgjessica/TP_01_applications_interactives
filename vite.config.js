de0c5bb94b69965766a75abe6e2c8454e17300d3
jpgjessica <jpg_jessica@hotmail.com>
2024-10-30 15:46:36 -0400

    configuration pour deploy sur vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { resolve } from 'path'
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, "dist");
+
 export default {
-    root: resolve(__dirname, 'src'),
+    root: root,
     build: {
-        outDir: '../dist'
+        outDir: outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                _bestsScores: resolve(root, '../src/assets/pages/_bestsScores.html'),
+                _config: resolve(root, '../src/assets/pages/_config.html'),
+                _jeu: resolve(root, '../src/assets/pages/_jeu.html'),
+                _scoreFinal: resolve(root, '../src/assets/pages/_scoreFinal.html'),
+            }
+        }
     },
     server: {
         port: 8080
